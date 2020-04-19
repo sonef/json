8fd47999ec4521f832aa620d45c74436ed261220
sonef <63943240+sonef@users.noreply.github.com>
2020-04-19 12:10:23 +0700

    Add files via upload

---
+++ json.js
@@ -0,0 +1,46 @@
+myFunction([
+{
+"display": "HTML Tutorial",
+"url": "MoreApp1",
+"gambar": "kakek-nenek.jpg"
+},
+{
+"display": "CSS Tutorial",
+"url": "EMoreApp2",
+"gambar": "kakek-nenek.jpg"
+},
+{
+"display": "JavaScript Tutorial",
+"url": "MoreApp3",
+"gambar": "kakek-nenek.jpg"
+},
+{
+"display": "SQL Tutorial",
+"url": "MoreApp4",
+"gambar": "kakek-nenek.jpg"
+}
+])
+
+function RateLink() {
+  Website2APK.openExternal("https://play.google.com/store/apps/details?id=com.bismillah.audioalquran30juz");
+}
+
+function PrivacyLink() {
+  Website2APK.openExternal("https://sites.google.com/view/alquran-audio-terjemahan-30juz/privacy-policy");
+}
+
+function MoreApp1() {
+  Website2APK.openExternal("https://play.google.com/store/apps/details?id=com.bismillah.audioalquran30juz");
+}
+
+function MoreApp2() {
+  Website2APK.openExternal("https://play.google.com/store/apps/details?id=com.bismillah.audioalquran30juz");
+}
+
+function MoreApp3() {
+  Website2APK.openExternal("https://play.google.com/store/apps/details?id=com.bismillah.audioalquran30juz");
+}
+
+function MoreApp4() {
+  Website2APK.openExternal("https://play.google.com/store/apps/details?id=com.bismillah.audioalquran30juz");
+}
